cabc28fb1edc7d7ae4371feae63f7ac4f2f57672
Thomas Levine <_@thomaslevine.com>
2014-02-23 01:56:43 +0000

    move to worker

--- assets/worker.js
+++ assets/worker.js
@@ -6,3 +6,20 @@ var client = websocket('ws://localhost:9000')
 client = client.pipe(JSONStream.parse([true]))
 var events = emitStream(client)
 events.on('foo', function(data) { console.log(data) })
+
+
+function receive(event.data){
+    eval(event.data)(send)
+}
+
+function send(error, data){
+
+}
+
+function browserfarm_client(url) {
+  var ws = new WebSocket(url)
+  ws.onmessage = receive
+
+}
+
+// browserfarm_client(ws://browserfarm.thomaslevine.com)
